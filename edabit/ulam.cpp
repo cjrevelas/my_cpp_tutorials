0f0b1b75c5b9f757b4ce855ccd355709031fa483
Constantinos J. Revelas <cjrevelas@gmail.com>
2020-09-12 00:26:07 +0300

    Add files via upload

---
+++ edabit/ulam.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+
+int main(){
+    const int n = 495;
+
+    std::vector<int> vec;
+
+    vec.push_back(1);
+    vec.push_back(2);
+
+    for (int pp=3; pp<8000; ++pp){
+		int counter = 0;
+        for (int ii=0; ii<(int) vec.size()-1; ++ii){
+            for (int jj=ii+1; jj<(int) vec.size(); ++jj){
+                if (vec[ii]+vec[jj]==pp) {++counter;}
+				if (counter>1) {break;}
+            }
+			if (counter>1) {break;}
+        }
+
+		if (counter == 1) {vec.push_back(pp);}
+        if (vec.size()==n) {break;}
+    }
+
+
+    return vec.back();
+}
