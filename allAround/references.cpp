0cbc84ebbaf864930e63b3da14568fba9580d39f
cjrevelas <cjrevelas@gmail.com>
2022-04-15 10:17:36 +0300

    feat: add references

---
+++ allAround/references.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+
+// Declare Widget class
+class Widget {
+ public:
+  Widget() {
+    std::cout << "Widget has been created\n";
+  }
+
+  ~Widget() {
+    std::cout << "Widget has been destroyed\n";
+  }
+
+  Widget getWidget() {
+    return *this;
+  }
+
+  const Widget getConstWidget() {
+    return *this;
+  }
+};
+
+// Forward declarations of all possible functions
+void ff(Widget &);        // L-value reference
+void ff(const Widget &);  // L-value reference to const
+void ff(Widget &&);       // R-value reference
+void ff(const Widget &&); // R-value reference to const
+
+template<typename T>
+void ff(T &&); // Funtion template with R-value reference to const
+
+template<typename T>
+void ff(const T &&); // Function template with R-value reference to const
+
+int main() {
+
+  Widget ww1{};
+  ff(ww1); // try this with L-value reference function commented out
+
+  const Widget ww2{};
+  ff(ww2);
+
+  ff(ww1.getWidget());
+
+  ff(ww1.getConstWidget());
+
+  return 0;
+}
+
+// Define the bodies of the reference receiving functions
+void ff(Widget &) {
+  std::cout << "calling function 1: L-value reference\n\n";
+}
+
+void ff(const Widget &) {
+  std::cout << "calling function 2: L-value reference to const\n\n";
+}
+
+void ff(Widget &&) {
+  std::cout << "calling function 3: R-value reference\n\n";
+}
+
+void ff(const Widget &&) {
+  std::cout << "calling function 4: R-value reference to const\n\n";
+}
+
+template<typename T>
+void ff(T &&) {
+  std::cout << "calling function 5: templated function with forwarding reference\n\n";
+}
+
+template<typename T>
+void ff(const T &&) {
+  std::cout << "calling funtion 6: templated function with R-value reference to const\n\n";
+}
