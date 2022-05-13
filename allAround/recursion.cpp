6018e6d94ca3dc88badd71ccf8579ef360595e26
cjrevelas <cjrevelas@gmail.com>
2022-05-13 22:32:12 +0300

    docs: rename and remove some files

---
+++ allAround/recursion.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int factorial(int x) {
+  if (x > 1) {
+    std::cout << x << '\n';
+    return x*factorial(x - 1);
+  }
+  else {
+    return 1; // Base case
+  }
+}
+
+int main() {
+  int x = 5;
+  int y = factorial(x);
+
+  std::cout << "factorial = " << y << '\n';
+
+  std::cin.get();
+
+  return 0;
+}
