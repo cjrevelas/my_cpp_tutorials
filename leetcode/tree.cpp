8a9f837b742b1e7a61f5cc9cd715bd86e27f1d54
cjrevelas <cjrevelas@gmail.com>
2024-06-30 14:39:23 +0300

    feat: add method for max depth calculation

--- leetcode/tree.cpp
+++ leetcode/tree.cpp
@@ -1,4 +1,6 @@
 // Definition of a binary tree node
+#include <algorithm>
+
 struct TreeNode {
   int val_;
   TreeNode *left_;
@@ -7,3 +9,22 @@ struct TreeNode {
   TreeNode(int val) : val_(val), left_(nullptr), right_(nullptr) {}
   TreeNode(int val, TreeNode *left, TreeNode *right) : val_(val), left_(left), right_(right) {}
 };
+
+class Solution {
+  public:
+    int maxDepth(TreeNode *root) {
+      if (root == nullptr) return 0;
+
+      int depth_left  = maxDepth(root->left_);
+      int depth_right = maxDepth(root->right_);
+
+      return std::max(depth_left, depth_right) + 1;
+    }
+};
+
+int main() {
+
+  Solution solution;
+
+  return 0;
+}
