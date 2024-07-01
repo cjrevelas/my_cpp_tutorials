96765aff4d7e1343b3d2dd54d6e6686fec212b64
cjrevelas <cjrevelas@gmail.com>
2024-07-01 13:13:56 +0300

    feat: build tree from input array

--- leetcode/tree.cpp
+++ leetcode/tree.cpp
@@ -47,5 +47,36 @@ int main() {
 
   Solution solution;
 
+  std::vector<int> input{3,9,20,0,0,15,7};
+
+  TreeNode *tree = new TreeNode(input[0]);
+  TreeNode *root = tree;
+
+  for (int ii=0; ii<(input.size()-1)/2; ++ii) {
+    TreeNode *left  = new TreeNode(input[2*ii+1]);
+    TreeNode *right = new TreeNode(input[2*(ii+1)]);
+
+    tree->left_  = left;
+    tree->right_ = right;
+
+    if (input[2*ii+1] == 0)   tree->left_ = nullptr;
+    if (input[2*(ii+1)] == 0) tree->right_ = nullptr;
+
+    if (tree->left_ == nullptr || tree->right_ == nullptr) {
+      tree = root;
+    }
+
+    if ((ii+1) % 2 != 0) {
+      tree = tree->left_;
+    } else {
+      tree = tree->right_;
+    }
+  }
+
+  solution.printTree(root);
+
+  std::cout << '\n';
+  std::cout << "Max depth equal to: " << solution.maxDepth(root) << '\n';
+
   return 0;
 }
